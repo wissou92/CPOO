f96e6e347fb9d5284c1498c1267e47bc49a11e32
Utilisateur <user@debian.arcahe.ovh>
2019-03-20 16:40:22 +0100

    organisation du projet

---
+++ Exercice_6/Complexe/Main.java
@@ -0,0 +1,15 @@
+import org.apache.commons.math3;
+/**
+ * Décrivez votre classe Main ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Main
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    public static void main(String[] arg)
+    {
+        
+    }
+}
